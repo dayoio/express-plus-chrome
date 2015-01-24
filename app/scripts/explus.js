3d0cef8830e6268d0f2c96a1bd2ffaaef9726c04
Siuzuk Zan <minoscc+github@gmail.com>
2015-01-24 16:05:52 +0800

    rewriting service

--- app/scripts/explus.js
+++ app/scripts/explus.js
@@ -10,87 +10,118 @@
  });*/
 
 angular.module('explus', ['ngResource'])
-    .factory('Post', function ($resource, $q) {
+    .factory('Post', function ($q) {
+
+        var Post = function (id, com) {
+            this.id = id;
+            this.com = com;
+        }
+        Post.prototype = {
+            setData: function (d) {
+                angular.extend(this, d);
+                /*this.check = (d['ischeck'] === '1')
+                this.steps = d['data'] || [];
+                 this.status = d['status'];
+                 */
+                if(d.data& d.data.length>0) {
+                    var a = new Date(d.data[d.data.length - 1].time)
+                    var b = new Date(d.data[0].time);
+                    this.totaltime = (b.getTime() - a.getTime());
+                }
+            },
+            toSimple: function(){
+                var sp = {};
+                sp.id = this.id;
+                sp.com = this.com;
+                sp.last = this.data[0].context;
+                sp.time = this.data[0].time;
+                sp.check = (this.ischeck === '1');
+                return sp;
+            }
+        }
+
+        return Post;
+    })
+    .factory('postsService', function ($q, $resource, $window, $rootScope, Post) {
+        var postsService;
         var _Auto = $resource('http://www.kuaidi100.com/autonumber/auto?num=:postid', {postid: '@id'});
         var _Query = $resource('http://www.kuaidi100.com/query?type=:type&postid=:postid', {
             type: '@type',
             postid: '@id'
         });
 
-        var Post = function (id) {
-            this.id = id;
-            this.com = null;
-            this.text = '';
-            this.totaltime = 0;
-            this.check = false;
-        }
+        angular.element($window).on('storage', function(event){
+           if(event.key === 'marks'){
+               $rootScope.$apply();
+           }
+        });
 
-        Post.prototype = {
-            _setData: function (d) {
-                this.check = (d['ischeck'] === '1')
-                this.steps = d['data'];
-                var a = new Date(d.data[d.data.length - 1].time)
-                var b = new Date(d.data[0].time);
-                this.totaltime = (b.getTime() - a.getTime());
-            },
-            _define: function () {
-                var defer = $q.defer();
-                if (this.com) {
-                    defer.resolve(this.com);
+        postsService = {
+            _posts: {},
+            _retrieve: function (id, com) {
+                var post = this._posts[id];
+                if (post) {
+                    post.com = com;
                 } else {
-                    _Auto.query({postid: this.id},
-                        function (data) {
+                    post = new Post(id, com);
+                    this._posts[id] = post;
+                }
+                return post;
+            },
+            _search: function (id) {
+                return this._posts[id];
+            },
+            _define: function (id) {
+                var scope = this;
+                var defer = $q.defer();
+                var post = this._search(id);
+                if (post) {
+                    defer.resolve(post);
+                } else {
+                    _Auto.query({postid: id}, function (data) {
+                            console.log(data);
                             if (data.length > 0) {
-                                defer.resolve(data[0].comCode);
+                                var post = scope._retrieve(id, data[0].comCode);
+                                return defer.resolve(post);
                             } else {
-                                defer.reject();
+                                return defer.reject({status:'400', message:'Is not a valid post id.'});
                             }
                         },
                         function (error) {
-                            defer.reject(error);
+                            defer.reject({status:'400', message: 'Is not a valid post id.', error: error});
+                            return;
                         });
                 }
                 return defer.promise;
             },
-            update: function () {
+            update: function (post) {
                 var defer = $q.defer();
-                var scope = this;
-                console.log('start define');
-                this._define().then(
-                    function (com) {
-                        console.log('define success: ' + com);
-                        scope.com = com;
-                        _Query.get({type: com, postid: scope.id},
-                            function (data) {
-                                scope._setData(data);
-                                defer.resolve(scope);
-                            },
-                            function (error) {
-                                defer.reject(error);
-                            });
+                _Query.get({type: post.com, postid: post.id},
+                    function (data) {
+                        post.setData(data);
+                        if(!data.data) post.message = 'Data not found!! Please try again later.'
+                        defer.resolve(post);
                     },
                     function (error) {
-                        defer.reject(error);
-                    }
-                )
+                        defer.reject({status:'400', message:'', error: error});
+                    });
                 return defer.promise;
-            }
-        }
-
-        return Post;
-    })
-    .factory('postsService', function ($q, Post) {
-        var postsService;
-
-        postsService = {
-            _posts: {},
-            _search: function(id){
-                return this._posts[id];
             },
-            _load: function(id){
+            getPost: function (id) {
+                return this._define(id)
+            },
+            saveMark: function(id){
+                var post = this._search(id);
+                if(post){
+                    //
+                }else{
+
+                }
+            },
+            removeMark: function(id){
 
             },
-            getPost: function(id){
+            getAllMarks: function(){
 
             }
         }
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,106 +1,44 @@
 'use strict';
 
 /**
-* PopupApp Module
-* 彈出窗口模塊
-*/
+ * PopupApp Module
+ * 彈出窗口模塊
+ */
 
-angular.module('popupApp', ['explus']).controller('MainController', function($scope, postsService){
+angular.module('popupApp', ['explus']).controller('MainController', function ($scope, postsService) {
 
-    //狀態
-    $scope.status = "init"
-    $scope.Status = {
-        "init": "init",
-        "loading": "loading",
-        "ready": "ready",
-        "error": "error"
-    }
+    $scope.loading = false;
 
     //查詢方法
-    $scope.query = function(){
-        $scope.status = "loading"
-
-        postsService.getPost($scope.postId).then(function(post){
-            $scope.post = post;
-            $scope.status = 'ready';
-        },function(error){
-            $scope.status = 'error';
-            console.log(error);
-        })
-
-        //$scope.subed = (optionsService.getSub($scope.postId).index !== -1);
-        //if($scope.postId == undefined ) return;
-        /*uery.query($scope.postId, function(d){
-            console.log(d);
-            if(d.status == "200")
-            {
-                $scope.status = 'ready';
-                $scope.comCode = d.com;
-                //
-                var a = new Date(d.data[d.data.length-1].time)
-                var b = new Date(d.data[0].time);
-                $scope.pTime = (b.getTime()-a.getTime());
-                $scope.result = d.data;
-                $scope.ischeck = (d.ischeck === "1")
-                $scope.nothing = false;
-                if($scope.subed)
-                {
-                    var tmp = optionsService.getSub(d.nu);
-                    if(tmp.index != -1 && tmp.value.last != d.data[0].time )
-                    {
-                        tmp.value.last = d.data[0].time;
-                        tmp.value.text = d.data[0].context;
-                        tmp.value.check = (d.ischeck === "1");
-                        optionsService.addSub(tmp.value);
-                    }
-                }
-            }
-            else if(d.status == "201")
-            {
-                $scope.status = 'ready';
-                $scope.comCode = d.com;
-                $scope.result = {
-                    "message": d.message
-                }
-                $scope.nothing = true;
-            }
-            else if( d.status == "400" || d.status == '500')
-            {
-                $scope.status = "error";
-                $scope.result = d;
-            }
-        });*/
+    $scope.query = function () {
+        $scope.loading = true;
+        $scope.post = {}
+        postsService.getPost($scope.postId).then(postsService.update).then(
+            function (post) {
+                console.log(post);
+                $scope.post = post;
+                $scope.loading = false;
+            }, function (error) {
+                console.log(error);
+                $scope.post = error;
+                $scope.loading = false;
+            })
     };
 
-    $scope.makerss = function(){
-        /*var item = {
-            "id": $scope.postId,
-            "com": $scope.comCode,
-            "last": !$scope.nothing ? $scope.result[0].time : "",
-            "text": !$scope.nothing ? $scope.result[0].context : "",
-            "check": $scope.ischeck,
-            "tags": ""
-        }
-        $scope.subed = !$scope.subed;
-        if($scope.subed == true)
-        {
-            optionsService.addSub(item);
-        }else{
-            optionsService.removeSub(item.id);
-        }*/
+    $scope.mark = function () {
+
     };
 
-}).filter('spendTime', function(){
-    return function(value){
-        if(!value) "0天";
+}).filter('spendTime', function () {
+    return function (value) {
+        if (!value) "0天";
         value = Math.abs(value);
 
         var res = "";
-        var hh = Math.floor(value/1000/60/60);
-        if(hh>=24)
-        {
-            var dd = Math.floor(hh/24);
-            hh -= dd*24;
+        var hh = Math.floor(value / 1000 / 60 / 60);
+        if (hh >= 24) {
+            var dd = Math.floor(hh / 24);
+            hh -= dd * 24;
             res = dd + '天';
         }
         res += hh + '小時';
